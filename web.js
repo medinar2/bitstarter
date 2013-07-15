277dc09ed43e206ee7430aff26bb62812dfb0678
medinar2 <rmedinajr2@gmail.com>
2013-07-15 03:54:19 +0000

    updated web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request) {});
 
-fs.readFileSync('/home/ubuntu/bitstarter/index.html', function (err, data) {
+fs.readFile('index.html', function (err, data) {
     if (err) throw err;
     console.log(data);
 });
